630eafb3c156ee681bf7d3682e7994ff80fe9f5e
Thomas Chamelot <chamelot.thomas@gmail.com>
2020-11-14 10:43:20 +0100

    handle spells. bronze 1

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Witch:
         Select the action to do
         """
         target = actions.best(self.inventory)
-        print(f'BREW {target}')
+        print(target)
 
 
 class Actions:
@@ -44,11 +44,13 @@ class Actions:
         """
         nb_action = int(input())
         self.orders = {}
+        self.ally_cast = {}
+        self.enemy_cast = {}
         for _ in range(nb_action):
             aid, atype, d0, d1, d2, d3, price, _, _, castable, _ = input().split()
-            if castable and atype == "CAST":
+            if castable == "1" and atype == "CAST":
                 self.ally_cast[aid] = np.array([int(d0), int(d1), int(d2), int(d3)])
-            elif castable and atype == "OPPONENT_CAST":
+            elif castable == "1" and atype == "OPPONENT_CAST":
                 self.enemy_cast[aid] = np.array([int(d0), int(d1), int(d2), int(d3)])
             elif atype == "BREW":
                 self.orders[aid] = {
@@ -66,10 +68,16 @@ class Actions:
             reverse=True))
 
         for order_id, order in self.orders.items():
-            if (inventory - order['recipe']).min() >= 0:
-                return order_id
+            if (inventory + order['recipe']).min() >= 0:
+                return f'BREW {order_id}'
 
-        return {}
+        for cast_id, cast in self.ally_cast.items():
+            new_inventory = inventory + cast
+            # print(new_inventory, file=sys.stderr)
+            if new_inventory.sum() <= 10 and new_inventory.min() >= 0:
+                return f'CAST {cast_id}'
+
+        return 'REST'
 
 
 
